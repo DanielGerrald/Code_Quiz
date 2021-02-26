d6930c0c0ed56030abc639385e55aeb4c8ac7cd4
Daniel Gerrald <daniel.gerrald@icloud.com>
2021-02-26 16:41:36 -0500

    corrected local storage function

--- score.js
+++ score.js
@@ -1,25 +1,26 @@
-var score;
-var user;
+let highScores;
+let arr = [];
 
+document.getElementById("Clear").onclick = function () {
+  document.getElementById("list").innerHTML = "";
+  window.localStorage.clear();
+  console.log("done");
+};
 
-
-if (localStorage.getItem("highScores") !== null){
-  highScores = JSON.parse(localStorage.getItem("highScores"));
-renderScores();
+if (localStorage.getItem("highScores") !== null) {
+  highScores = localStorage.getItem("highScores");
+  arr = highScores.split(",");
+  // console.log(arr);
+  renderScores();
 }
 
 function renderScores() {
-    for (var i = 0; i < highScores.length;) {
-      var li = document.createElement("li");
-      li.textContent = (highScores[i] + " - " + highScores[i +1]);
-      li.setAttribute("class", "list-group-item");
-      li.setAttribute("data-index", i);
-      i += 2;
-      list.appendChild(li);
-    }
+  for (var i = 0; i < arr.length; ) {
+    var li = document.createElement("li");
+    li.textContent = arr[i] + " - " + arr[i + 1];
+    li.setAttribute("class", "list-group-item  list-group-item-action list-group-item-warning");
+    li.setAttribute("data-index", i);
+    i += 2;
+    list.appendChild(li);
   }
-
-  document.getElementById("Clear").onclick = function(){
-    document.getElementById("list").innerHTML = "";
-    localStorage.clear();
-  };
+}
--- script.js
+++ script.js
@@ -1,40 +1,62 @@
-var seconds=60;
+var seconds = 60;
 var timer;
 var questionEl = document.getElementById("question");
 var answerA = document.getElementById("A");
 var answerB = document.getElementById("B");
 var answerC = document.getElementById("C");
 var answerD = document.getElementById("D");
-var cardBody = document.querySelector(".card-body")
-var userForm = document.querySelector("#user-form")
-var initials = document.getElementById("initials")
+var cardBody = document.querySelector(".card-body");
+var userForm = document.querySelector("#user-form");
+var initials = document.getElementById("initials");
 var highScores = [];
 
-
-
 var quiz = {
-  question:["From what King’s Cross platform does the Hogwarts Express leave?","Who guards the entrance to the Gryffindor common room?","Which is NOT a form of currency in the wizarding world?","What does Mrs. Weasley give Harry for Christmas every year?","What is the model of the first broom Harry ever receives?"],
-  answers:["A) Eight and One-quarter", "B) Nine and Three-quarters", "C) Five and a Half", "D) Eleven","A) The Grey Lady", "B) The Fat Friar", "C) The Bloody Baron", "D) The Fat Lady","A) Doxies", "B) Sickles", "C) Knuts", "D) Galleons","A) Bertie Bott’s every flavour beans", "B) Chocolate frogs", "C) A fruit cake", "D) A new sweater","A) Cleansweep One", "B) Nimbus 2000", "C) Hoover", "D) Firebolt"],
-  correct:["B", "D", "A", "D", "B"],
-}   
-
-
+  question: [
+    "From what King’s Cross platform does the Hogwarts Express leave?",
+    "Who guards the entrance to the Gryffindor common room?",
+    "Which is NOT a form of currency in the wizarding world?",
+    "What does Mrs. Weasley give Harry for Christmas every year?",
+    "What is the model of the first broom Harry ever receives?",
+  ],
+  answers: [
+    "A) Eight and One-quarter",
+    "B) Nine and Three-quarters",
+    "C) Five and a Half",
+    "D) Eleven",
+    "A) The Grey Lady",
+    "B) The Fat Friar",
+    "C) The Bloody Baron",
+    "D) The Fat Lady",
+    "A) Doxies",
+    "B) Sickles",
+    "C) Knuts",
+    "D) Galleons",
+    "A) Bertie Bott’s every flavour beans",
+    "B) Chocolate frogs",
+    "C) A fruit cake",
+    "D) A new sweater",
+    "A) Cleansweep One",
+    "B) Nimbus 2000",
+    "C) Hoover",
+    "D) Firebolt",
+  ],
+  correct: ["B", "D", "A", "D", "B"],
+};
 
 function timerStart() {
-  if(seconds <= 60) {
+  if (seconds <= 60) {
     document.getElementById("timer").innerHTML = seconds;
   }
   if (seconds > 0 && ques <= 3) {
     seconds--;
-  } 
-  else {
+  } else {
     clearInterval(timer);
     return;
   }
 }
-document.getElementById("start").onclick = function() {
-  if(!timer) {
-    timer = window.setInterval(function() {
+document.getElementById("start").onclick = function () {
+  if (!timer) {
+    timer = window.setInterval(function () {
       document.getElementById("start").style.display = "none";
       document.getElementById("navTimer").removeAttribute("style");
       answerA.removeAttribute("style");
@@ -45,42 +67,39 @@ document.getElementById("start").onclick = function() {
       replace();
     }, 1000);
   }
-}
+};
 
 var ques = 0;
 var choi = 0;
 
-cardBody.addEventListener("click", function(event) {
+cardBody.addEventListener("click", function (event) {
   event.preventDefault();
   if (event.target.matches("button") === true) {
     var theirAnswer = event.target.getAttribute("id");
     //console.log(theirAnswer)
   }
-  if (theirAnswer === quiz.correct[ques] && ques <=3 && seconds > 0){
+  if (theirAnswer === quiz.correct[ques] && ques <= 3 && seconds > 0) {
     ques++;
     choi += 4;
     replace();
     //console.log(choi)
-  }
-  else if (ques <= 3 && seconds > 0){
+  } else if (ques <= 3 && seconds > 0) {
     ques++;
     choi += 4;
     seconds -= 15;
     replace();
-  }
-  else {
-    answerA.style.display = 'none';
-    answerB.style.display = 'none';
-    answerC.style.display = 'none';
-    answerD.style.display = 'none';
+  } else {
+    answerA.style.display = "none";
+    answerB.style.display = "none";
+    answerC.style.display = "none";
+    answerD.style.display = "none";
     userForm.removeAttribute("style");
   }
 });
-  
-userForm.addEventListener("click", function(event) {
+
+userForm.addEventListener("click", function (event) {
   event.preventDefault();
   if (event.target.matches("button") === true) {
-
     storeHighScores();
   }
 });
@@ -88,21 +107,20 @@ userForm.addEventListener("click", function(event) {
 function replace() {
   questionEl.innerHTML = quiz.question[ques];
   answerA.innerHTML = quiz.answers[choi];
-  answerB.innerHTML = quiz.answers[choi + 1]; 
+  answerB.innerHTML = quiz.answers[choi + 1];
   answerC.innerHTML = quiz.answers[choi + 2];
   answerD.innerHTML = quiz.answers[choi + 3];
-};
-
-function storeHighScores() {
- // var temp = localStorage.getItem("highScores");
- // if (temp !== null) {
- //   highScores.push(temp)
- //   console.log(temp)
- // }
-      highScores.push(initials.value,seconds);
-      //highScores.push(seconds);
-  console.log(highScores);
-
-  localStorage.setItem("highScores", highScores);
 }
 
+function storeHighScores() {
+  var temp = localStorage.getItem("highScores");
+  if (temp !== null) {
+    highScores.push(temp)
+    // console.log(temp)
+  }
+  highScores.push(initials.value, seconds);
+  // console.log(highScores);
+
+  localStorage.setItem("highScores", highScores);
+  window.location.href = "Score.html";
+}
